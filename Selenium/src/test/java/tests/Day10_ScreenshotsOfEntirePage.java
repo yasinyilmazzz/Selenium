8bf080682e567724f824a3ee54d00c66b61595f0
Yasin Yılmaz <yasinyilmazzz@gmail.com>
2022-12-30 19:18:24 +0300

    v1

---
+++ Selenium/src/test/java/tests/Day10_ScreenshotsOfEntirePage.java
@@ -0,0 +1,48 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+import java.io.IOException;
+
+public class Day10_ScreenshotsOfEntirePage extends TestBase {
+
+//        "https://testcenter.techproeducation.com/index.php?page=autocomplete"
+//        Given user search for ‘uni’
+//        And select United Kingdom
+//        Add click on submit button
+//        Then verify the result contains united Kingdom
+
+    @Test
+    public void takeScreenShot() throws IOException {
+//        Given user search for ‘uni’
+        driver.get("https://testcenter.techproeducation.com/index.php?page=autocomplete");
+
+//      TAKE A SCREENSHOT. CREATE A REUSABLE METHOD AND CALL THAT METHOD
+        takeScreenshotOfPage();
+
+
+//  ========================================= ********** =========================================
+//       COMPLETE AS HOMEWORK
+//        And select United Kingdom
+//        searchAndSelectFromList("uni","United Kingdom");
+//        When user type “uni” in the search box
+        driver.findElement(By.id("myCountry")).sendKeys("uni");
+//       And select the ‘United Kingdom’ from the suggestions
+        driver.findElement(By.xpath("//div[@id='myCountryautocomplete-list']//div[.='United Kingdom']")).click();
+//        TAKE SCREENSHOT
+        takeScreenshotOfPage();
+//        Add click on submit button
+        //    And click on submit button
+        driver.findElement(By.xpath("//input[@type='button']")).click();
+//        TAKE SCREENSHOT
+        takeScreenshotOfPage();
+//        Then verify the result contains united Kingdom
+        //    Then verify the result contains ‘United Kingdom’
+        Assert.assertTrue(driver.findElement(By.id("result")).getText().contains("United Kingdom"));
+//        TAKE SCREENSHOT
+        takeScreenshotOfPage();
+    }
+}
